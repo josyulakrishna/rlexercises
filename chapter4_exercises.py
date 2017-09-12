a59e2f5e4fc3661ab3b2bcc2a308be2843a57ee4
Josyula Gopala Krishna <josyula008@gmail.com>
2017-09-12 19:52:16 +0530

    getting possible next state adding valueiteration

--- chapter4_exercises.py
+++ chapter4_exercises.py
@@ -1,11 +1,22 @@
 import numpy as np
 from collections import defaultdict
+import chapter_4_environments.grid_environment
 
-def iterative_policy_evalutation(policy, states, actions): 
+#generic value iteration 
+def iterative_policy_evalutation(policy, states, actions, theta, gamma): 
     V = defaultdict(float)
     v = defaultdict(float)
-    while: 
-       deltas = 0
+    ge = chapter_4_environments.grid_environment()
+    delta = 0
+    while(delta<theta): 
        for i in len(states): 
-           v[states[i]] = V[states[i]]
-           V[states[i]] = [for a in actions]
+            v[states[i]] = V[states[i]]
+            # Reward for action 
+            # Possible actions actions=[0:up 1:down 2:left 3:right]
+            # Here one action is chosen randomly on  a uniform distribution
+            a = np.random.choice([0,1,2,3], 1)
+            s_ = ge.get_next_state(s, a)
+            r = ge.reward(i, a, s_)
+            p_a_s = ge.tranisiton_probabilities(s, r, a, s_)
+            V[states[i]] = p_a_s*(r+gamma*v[s_])
+            delta  = max(delta, abs(v[s_]-V[s_]))
--- chapter_4_environments.py
+++ chapter_4_environments.py
@@ -6,7 +6,7 @@ class grid_environment:
     def __init__(self):  
         self.states = range(15)
         self.actions = range(4)
-
+#returns +1 for all possible transitions and 0 for rest 
     def tranisiton_probabilities(self, s, r, a, s_):
         pos_s=[s, s-4, s+4, s-1, s+1]
         if s_ in pos_s:
@@ -26,6 +26,10 @@ class grid_environment:
         else: 
             return 0
 
+
+#Returns -1 reward for all possible transitions, i.e up, down, left, right
+#stay in the same block(only for boundary boxes or cells near an obstacle)
+#except the goal which has reward of 0
     def reward(self, s, a, s_):
         print s_, a, s
         pos_s=[s, s-4, s+4, s-1, s+1]
@@ -37,3 +41,26 @@ class grid_environment:
             return -1
         else:
             return 0
+
+    def get_next_state(self, s, a):
+        # actions=[up,0 down,1 left,2 right,3 ]
+            pos_s = None
+            if a==0 and s>1 and s<5:
+                pos_s=s
+            elif a==3 and s%8==0:
+                pos_s=s
+            elif a==2 and s%8==1:
+                pos_s=s
+            elif a==1 and s>12 and s<15
+                pos_s=s
+            else:
+                #pos_s=[(bump):s, 0:up:s-4, 1:down:s+4, 2:left:s-1, 3:right:s+1]
+                if a==0:
+                    pos_s = s-4
+                if a==1: 
+                    pos_s = s+4
+                if a==2:
+                    pos_s = s-1
+                if a==3:
+                    pos_s = s+1
+            return pos_s
